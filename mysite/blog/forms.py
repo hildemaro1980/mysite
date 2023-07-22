af9fc49a7cf79c240465803dea926bf8b82ca1b4
hildemaro1980 <hildemaro1980@gmail.com>
2023-07-21 22:48:36 -0300

    From VS Code!

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -4,4 +4,4 @@ class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
-    coments = forms.CharField(required = False, widget=forms.Textarea)
+    comments = forms.CharField(required = False, widget=forms.Textarea)
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -41,10 +41,17 @@ def post_detail(request, year, month, day, post):
  
 def post_share(request, post_id):
     post = get_object_or_404(Post, id = post_id, status = Post.Status.PUBLISHED)
+    sent = False
+
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f" Read {post.title} at {post_url}\n\n {cd['name']}\'s comments: {cd['comments']}"
+            send_mail = (subject, message, 'hildemaro1980@gmail.com', [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post,'form':form})
+    return render(request, 'blog/post/share.html', {'post': post,'form':form, 'sent': sent})
